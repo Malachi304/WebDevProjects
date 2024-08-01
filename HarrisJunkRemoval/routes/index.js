50ff00f151c3b0d071405bf5fa43edb11989f116
Malachi <mpeel9938@gmail.com>
2024-08-01 02:55:48 -0700

    new flex layout for middle content // resized main image

---
+++ HarrisJunkRemoval/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+const needle = require('express')
+
+router.get('/', (req, res) => {
+    const apiRes = await needle('get',)
+})
+  
+
+module.exports = router
